ec64d10c2af0b05c29abcc33512ce96f8533d4e6
o-su <9551674+o-su@users.noreply.github.com>
2020-06-03 22:43:03 +0200

    implemented simple crawler

--- src/main/kotlin/WebSpider.kt
+++ src/main/kotlin/WebSpider.kt
@@ -4,11 +4,70 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 
 fun main () {
-    println("test")
+    val webSpider = WebSpider(WebSpiderSettings(userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"))
+
+    webSpider.run("https://kotlinlang.org/")
 }
 
-fun getContentFromUrl(url: String): Document {
-    val connection = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246")
+data class WebSpiderSettings(val userAgent: String)
 
-    return connection.get()
+class WebSpider(private val settings: WebSpiderSettings) {
+    private val index: ArrayList<String> = ArrayList()
+    private val queue: ArrayList<String> = ArrayList()
+    private var running: Boolean = false
+
+    fun run(url: String) {
+        this.index.add(url)
+        this.queue.add(url)
+        this.crawl()
+        this.running = true
+    }
+
+    fun stop() {
+        this.running = false
+    }
+
+    fun getIndex(): ArrayList<String> {
+        return this.index
+    }
+
+    private fun crawl() {
+        val url = this.getFirstUrlFromQueue()
+        println(url)
+
+        if (url.isNotEmpty() && this.running) {
+            val content: Document = this.getPageContent(url)
+            val links = this.parseLinksFromPageContent(content)
+
+            this.addLinksToIndex(links)
+            this.addLinksToQueue(links)
+            this.crawl()
+         }
+    }
+
+    private fun getPageContent(url: String): Document {
+        val connection = Jsoup.connect(url).userAgent(this.settings.userAgent)
+
+        return connection.get()
+    }
+
+    private fun parseLinksFromPageContent(content: Document): List<String> {
+        return content.select("a[href]").map { link -> link.absUrl("href") }
+    }
+
+    private fun addLinksToIndex(links: List<String>) {
+        for (link in links) {
+            this.index.add(link)
+        }
+    }
+
+    private fun addLinksToQueue(links: List<String>) {
+        for (link in links) {
+            this.queue.add(link)
+        }
+    }
+
+    private fun getFirstUrlFromQueue(): String {
+        return this.queue.removeAt(0)
+    }
 }
--- src/test/kotlin/WebSpiderTest.kt
+++ src/test/kotlin/WebSpiderTest.kt
@@ -1,11 +1,20 @@
 
 import org.junit.Test
-import webSpider.getContentFromUrl
+import webSpider.WebSpider
+import webSpider.WebSpiderSettings
 import kotlin.test.assertEquals
-import kotlin.test.assertNotNull
 
-class TestSource {
-    @Test fun testGetContentFromUrl() {
-        assertNotNull(getContentFromUrl("https://kotlinlang.org/"))
+class WebSpiderTest {
+    @Test fun integrationTest() {
+        // given
+        val webSpider = WebSpider(WebSpiderSettings(userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246"))
+        val url = "https://kotlinlang.org/"
+
+        // when
+        webSpider.run(url)
+        webSpider.stop()
+
+        // then
+        assertEquals(webSpider.getIndex().first(), url)
     }
 }

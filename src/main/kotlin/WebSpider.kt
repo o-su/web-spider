5224e165a1197e9ebd748f05acab58420b65f35f
o-su <9551674+o-su@users.noreply.github.com>
2020-06-06 13:47:39 +0200

    added configuration for maximal depth limit and debug mode

--- src/main/kotlin/WebSpider.kt
+++ src/main/kotlin/WebSpider.kt
@@ -1,27 +1,40 @@
 package webSpider
 
+import kotlinx.coroutines.ExecutorCoroutineDispatcher
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import java.io.IOException
 import java.util.concurrent.Executors
-import kotlinx.coroutines.*
 
- fun main() = runBlocking {
-    val webSpider = WebSpider(WebSpiderSettings(userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (dispatcherKHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246", threadCount = 4))
+fun main() = runBlocking {
+    val webSpider = WebSpider(
+        WebSpiderSettings(
+            userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (dispatcherKHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+            threadCount = 4,
+            maxDepth = 3,
+            debug = true
+        )
+    )
 
      webSpider.run("https://kotlinlang.org/")
  }
 
-data class WebSpiderSettings(val userAgent: String, val threadCount: Int)
+data class WebSpiderSettings(val userAgent: String, val threadCount: Int, val maxDepth: Short?, val debug: Boolean)
+data class Link(val url: String, val depth: Int)
 
 class WebSpider(private val settings: WebSpiderSettings) {
     private val index: ArrayList<String> = ArrayList()
-    private val queue: ArrayList<String> = ArrayList()
+    private val queue: ArrayList<Link> = ArrayList()
     private var running: Boolean = true
     private val dispatcher: ExecutorCoroutineDispatcher = Executors.newFixedThreadPool(settings.threadCount).asCoroutineDispatcher()
+    private val errorLog: ArrayList<String> = ArrayList()
 
     suspend fun run(url: String) {
         index.add(url)
-        queue.add(url)
+        queue.add(Link(url = url, depth = 0))
         running = true
         processQueue()
     }
@@ -35,28 +48,48 @@ class WebSpider(private val settings: WebSpiderSettings) {
         return this.index
     }
 
+    fun getErrorLog(): ArrayList<String> {
+        return this.errorLog
+    }
+
     private suspend fun processQueue() {
         while(queue.isNotEmpty() && running) {
             crawl()
         }
+        stop()
     }
 
     private suspend fun crawl() =  withContext(this.dispatcher) {
-        val url = getFirstUrlFromQueue()
+        val link = getFirsLinkFromQueue()
 
-        if (!url.isNullOrEmpty()) {
-            val content: Document = getPageContent(url)
-            val links = parseLinksFromPageContent(content)
+        if (link !== null && settings.maxDepth !== null && link.depth <= settings.maxDepth) {
+            val document: Document? = getPageContent(link.url)
 
-            addLinksToIndex(links)
-            addLinksToQueue(links)
+            if (settings.debug) {
+                println(link.url + link.depth)
+            }
+
+            if (document !== null) {
+                val urls = parseLinksFromPageContent(document)
+                val depth = link.depth + 1
+
+                addLinksToIndex(urls)
+                addLinksToQueue(urls.map { url -> Link(url, depth) })
+            }
          }
     }
 
-    private fun getPageContent(url: String): Document {
+    private fun getPageContent(url: String): Document? {
         val connection = Jsoup.connect(url).userAgent(this.settings.userAgent)
+        var document: Document? = null
 
-        return connection.get()
+        try {
+            document = connection.get()
+        } catch (exception: IOException) {
+            this.logError(exception.toString())
+        }
+
+        return document
     }
 
     private fun parseLinksFromPageContent(content: Document): List<String> {
@@ -69,17 +102,21 @@ class WebSpider(private val settings: WebSpiderSettings) {
         }
     }
 
-    private fun addLinksToQueue(links: List<String>) {
+    private fun addLinksToQueue(links: List<Link>) {
         for (link in links) {
             this.queue.add(link)
         }
     }
 
-    private fun getFirstUrlFromQueue(): String? {
+    private fun getFirsLinkFromQueue(): Link? {
         if (this.queue.isNotEmpty()) {
             return this.queue.removeAt(0)
         }
 
         return null
+    }
+
+    private fun logError(message: String) {
+        this.errorLog.add(message)
     }
 }
--- src/test/kotlin/WebSpiderTest.kt
+++ src/test/kotlin/WebSpiderTest.kt
@@ -10,7 +10,14 @@ import kotlin.test.assertEquals
 class WebSpiderTest {
     @Test fun integrationTest() = runBlocking {
         // given
-        val webSpider = WebSpider(WebSpiderSettings(userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246", threadCount = 1))
+        val webSpider = WebSpider(
+            WebSpiderSettings(
+                userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
+                threadCount = 1,
+                maxDepth = 3,
+                debug = true
+            )
+        )
         val url = "https://kotlinlang.org/"
 
         // when
